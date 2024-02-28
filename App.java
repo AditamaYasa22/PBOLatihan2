eee23ebf6e7c3578993851c944401ec60650372a
Aditama Yasa <114473293+AditamaYasa22@users.noreply.github.com>
2024-02-28 23:13:59 +0700

    Add files via upload

---
+++ App.java
@@ -0,0 +1,16 @@
+package segitiga;
+
+public class App {
+    public static void main(String[] args) {
+        Segitiga segitiga1 = new Segitiga(6, 10);
+
+
+        double hasil = segitiga1.getLuas();
+        System.out.println("Luas segitiga adalah: " + hasil);
+        
+        // segitiga1.setAlas(7);
+        // segitiga1.setTinggi(12);
+        
+    
+    }
+}
